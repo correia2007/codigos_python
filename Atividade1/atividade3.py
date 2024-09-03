109444b76554fadd537f61bb324f47144b6e9ca4
correia <christiancorreiadeazevedo@gmail.com>
2024-09-03 10:55:55 -0300

    Duas atividades feitas no dia 03 de setembro

---
+++ Atividade1/atividade3.py
@@ -0,0 +1,6 @@
+altura = float(input("Informe sua altura: "))
+raio = float(input("Informe seu raio: "))
+
+volume = ((raio ** 2)* altura* 3.14159)
+
+print(f"Seu Volume é {volume:.2f}")
---
+++ Atividade1/atividade5.py
@@ -0,0 +1,7 @@
+altura = float(input("Informe sua altura: "))
+comprimento = float(input("Informe sua comprimento: "))
+largura = float(input("Informe seu largura: "))
+
+volume = (comprimento * altura* largura)
+
+print(f"Seu Volume é {volume:.2f}")
