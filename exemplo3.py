64e0f8f7393f836c94ae2bc6c2d86589c1967378
correia <christiancorreiadeazevedo@gmail.com>
2024-09-03 09:46:41 -0300

    Calculando média com python

---
+++ exemplo3.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Informe sua 1 nota: "))
+nota2 = float(input("Informe sua 2 nota: "))
+nota3 = float(input("Informe sua 3 nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"Sua média é {media:.2f}")
