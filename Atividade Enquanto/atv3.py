6393d1c7ea5b020a76e6f9b9b6bb378629677966
correia <christiancorreiadeazevedo@gmail.com>
2024-09-16 11:43:22 -0300

    Atividade 16/09

---
+++ Atividade Enquanto/atv3.py
@@ -0,0 +1,13 @@
+somaP = 0
+quantN = 0
+contador = 0
+while contador < 10:
+    valor = int(input("Digite um valor:"))
+    if valor > 0:
+        somaP += valor
+    elif valor < 0:
+        quantN += 1
+    contador += 1
+
+print(f"Soma dos Numeros positivos: {somaP} ")
+print(f"Quantidade de Numeros negativos: {quantN} ")
